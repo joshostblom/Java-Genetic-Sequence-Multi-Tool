95b9ea131d58ff12198082df1582a055cfae2c83
joshostblom <69378018+joshostblom@users.noreply.github.com>
2021-10-29 13:07:42 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //File file = new File("/Users/smart/Desktop/sequence.txt");
+        //Sequence sequence = new Sequence(true, 1, "DNA", file);
+    }
+}
