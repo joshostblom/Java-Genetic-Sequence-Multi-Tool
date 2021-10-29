7185e86d93fd94f87f28f6359c47e00f2e3c68e3
joshostblom <69378018+joshostblom@users.noreply.github.com>
2021-10-29 13:05:12 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Main {
-
-    public static void main(String[] args) throws FileNotFoundException {
-        //File file = new File("/Users/smart/Desktop/sequence.txt");
-        //Sequence sequence = new Sequence(true, 1, "DNA", file);
-    }
-}
