e022afe7dfd46840cfd04fd9688cf6088ed38bae
joshostblom <69378018+joshostblom@users.noreply.github.com>
2021-11-24 14:18:15 -0600

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Sequence"}];updateSearchResults();
